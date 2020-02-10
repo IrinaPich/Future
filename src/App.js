4243bec499c87f5953d5bec2d66e34194b3f97e0
Irina Pichugina <pichugina_i@mail.ru>
2020-02-10 16:23:59 +0000

    new component react-table-6

--- src/App.js
+++ src/App.js
@@ -1,73 +1,147 @@
 import React, { Component } from 'react';
 import './App.css';
-import { DataTable } from 'react-data-components';
+//import { DataTable } from 'react-data-components';
+import ReactTable from 'react-table-6'
+import 'react-table-6/react-table.css'
 
 
 function buildTable(data) {
 
-  const tableColumns = [
-    { title: 'id', prop: 'id' },
-    { title: 'firstName', prop: 'firstName' },
-    { title: 'lastName', prop: 'lastName' },
-    { title: 'email', prop: 'email' },
-    { title: 'phone', prop: 'phone' },
+
+
+
+
+  const columns = [
+    { Header: 'id', accessor: 'id' },
+    { Header: 'firstName', accessor: 'firstName' },
+    { Header: 'lastName', accessor: 'lastName' },
+    { Header: 'email', accessor: 'email' },
+    { Header: 'phone', accessor: 'phone' },
   ];
 
+
+
+
   return (
-    <DataTable
-      className="container"
-      keys="item + email"
-      columns={tableColumns}
-      initialData={data}
-      initialPageLength={5}
-      initialSortBy={{ prop: 'lastName', order: 'descending' }}
-      pageLengthOptions={[5, 20, 50]}
-    />
+    <div>
+      <ReactTable
+        className="container"
+        keys="email"
+        Header="Table"
+        columns={columns}
+        data={data}
+        resolveData={data => data.map(row => row)}
+        initialPageLength={5}
+        initialSortBy={{ prop: 'lastName', order: 'descending' }}
+        pageLengthOptions={[5, 20, 50]}
+        defaultPageSize={10}
+        progressPending={true}
+        filterable={true}
+
+
+        getTdProps={(state, rowInfo, column, instance) => {
+          return {
+            onClick: (e, handleOriginal) => {
+              console.log('It was in this row:', rowInfo)
+              console.log('!!!' + rowInfo.original.address.city)
+              document.getElementById("details").innerHTML = 
+              `Выбран пользователь <b> ${rowInfo.original.firstName}</b><br>
+               
+              Описание:<br><textarea>${rowInfo.original.description}</textarea><br>
+
+              Адрес проживания: <b>${rowInfo.original.address.streetAddress}</b>
+
+              <br>Город: <b>${rowInfo.original.address.city}</b><br>
+
+              Провинция/штат: <b>${rowInfo.original.address.state}</b><br>
+
+              Индекс: <b>${rowInfo.original.address.zip}</b>`;
+
+              if (handleOriginal) {
+                handleOriginal()
+              }
+            }
+          }
+        }}
+
+
+
+      />
+      <p id="details">Additional info of onClicked row</p>
+
+
+    </div>
+
   );
 }
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.checkData = this.checkData.bind(this);
     this.state = {
-      hideList: true,
       items: [],
-      id: -1,
-      showWindowPortal: false,
+      bigData: true,
+      error: null,
+      isLoaded: false,
     };
   }
 
+  checkData(e) {
+    this.setState(state => ({
+      bigData: !state.bigData
+    }));
+  }
+
   componentDidMount() {
-    fetch("http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}")
+    fetch(this.state.bigData ? "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}" : "http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}")
       .then(resp => resp.json())
       .then(
         (data) => {
-          //  console.log(data)
           this.setState({
-            items: data
+            isLoaded: true,
+            items: data,
+            bigData: false
           });
         },
+
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
       );
   }
-
-  //handleChange(id) {
-  //  this.setState(state => ({
-  //    hideList: !state.hideList,
-  //    id: id
-  //  }));
-
-  //}
-
-
 
   render() {
     console.log(this.state);
     const { items } = this.state;
     return (
-      buildTable(items)
+      <div>
+        <h1 className="container">Table</h1>
 
-      )
+        <form>
+        <p className="container">
+          <label>
+            <input
+              type="checkbox"
+              className="filled-in"
+              checked={this.props.inStockOnly}
+              onChange={this.checkData}
+            />
+            {' '}
+            <span>Big volume Data</span>
+          </label>
+        </p>
+      </form>
+
+        {buildTable(items)}
+
+      </div>
+    )
   }
 }
+
 
 export default App;
