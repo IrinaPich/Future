8a380540309a6a71bcea2381706e0f32c047cf44
Irina Pichugina <pichugina_i@mail.ru>
2020-02-11 09:55:36 +0000

    check the volume of Data

--- src/App.js
+++ src/App.js
@@ -44,8 +44,8 @@ function buildTable(data) {
             onClick: (e, handleOriginal) => {
               console.log('It was in this row:', rowInfo)
               console.log('!!!' + rowInfo.original.address.city)
-              document.getElementById("details").innerHTML = 
-              `Выбран пользователь <b> ${rowInfo.original.firstName}</b><br>
+              document.getElementById("details").innerHTML =
+                `Выбран пользователь <b> ${rowInfo.original.firstName}</b><br>
                
               Описание:<br><textarea>${rowInfo.original.description}</textarea><br>
 
@@ -81,38 +81,83 @@ class App extends Component {
     this.checkData = this.checkData.bind(this);
     this.state = {
       items: [],
-      bigData: true,
+      bigData: false,
       error: null,
       isLoaded: false,
     };
+    console.log("Loaded"+ this.state.bigData);
+    this.checkData = this.checkData.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+
   }
 
   checkData(e) {
+    console.log("onClick1"+ this.state.bigData);
+
+    this.fetchData()
+    
+   // fetchData(bigData);
     this.setState(state => ({
-      bigData: !state.bigData
+      
+      bigData: !this.state.bigData
     }));
+console.log("onClick2"+ this.state.bigData);
+
   }
+
+  fetchData() {
+
+    if (!this.state.bigData) {
+      console.log("32")
+      fetch("http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}")
+        .then(resp => resp.json())
+        .then(
+          (data) => {
+            this.setState({
+              isLoaded: true,
+              items: data,
+              bigData: true,
+
+            });
+          },
+
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        );
+    }
+    else if (this.state.bigData) {
+      console.log("1000")
+      fetch("http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}")
+
+        .then(resp => resp.json())
+        .then(
+          (data) => {
+            this.setState({
+              isLoaded: true,
+              items: data,
+              bigData: false,
+            });
+          },
+
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        );
+    }
+  }
+
 
   componentDidMount() {
-    fetch(this.state.bigData ? "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}" : "http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}")
-      .then(resp => resp.json())
-      .then(
-        (data) => {
-          this.setState({
-            isLoaded: true,
-            items: data,
-            bigData: false
-          });
-        },
-
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-      );
+    this.fetchData();
   }
+
 
   render() {
     console.log(this.state);
@@ -122,19 +167,19 @@ class App extends Component {
         <h1 className="container">Table</h1>
 
         <form>
-        <p className="container">
-          <label>
-            <input
-              type="checkbox"
-              className="filled-in"
-              checked={this.props.inStockOnly}
-              onChange={this.checkData}
-            />
-            {' '}
-            <span>Big volume Data</span>
-          </label>
-        </p>
-      </form>
+          <p className="container">
+            <label>
+              <input
+                type="checkbox"
+                className="filled-in"
+                checked={this.state.bigData}
+                onChange={this.checkData}
+              />
+              {' '}
+              <span>Small volume Data</span>
+            </label>
+          </p>
+        </form>
 
         {buildTable(items)}
 
